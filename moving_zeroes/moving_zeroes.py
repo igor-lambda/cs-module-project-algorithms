237e01599daedb966227ce36fec600e9e27bb576
igor <embiggenerd@gmail.com>
2020-05-20 18:05:38 -0400

    moving_zeroes passing

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,15 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    old_length = len(arr)
 
-    pass
+    # remove zero values and append zerped array to end
+    arr = [v for v in arr if v != 0]
+    new_length = len(arr)
+    zero_length = old_length - new_length
+
+    arr = arr + [0] * zero_length
+    return arr
 
 
 if __name__ == '__main__':
