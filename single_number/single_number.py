1cda99f1cf4b437fcfb252f994d94ba498dab7ae
igor <embiggenerd@gmail.com>
2020-05-20 17:42:32 -0400

    single_number passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,9 +4,15 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
-
+    amounts = {}
+    for n in arr:
+        if n in amounts:
+            amounts.pop(n)
+        else:
+            amounts[n] = None
+    print(amounts)
+    return list(amounts.keys())[0]
+    
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
